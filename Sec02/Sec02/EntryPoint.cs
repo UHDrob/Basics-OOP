ec840d3e8e7d343a0522d41ebfff58d1448f4041
Roberto Gomez <rob.77@live.com>
2020-11-05 21:55:53 -0600

    Section 3

---
+++ Sec02/Sec02/EntryPoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using PointsAndLines;
+
+namespace Sec02
+{
+
+   class EntryPoint
+   {
+      static void Main()
+      {
+         User user = new User("Vachev");
+
+         user.Password = 2;
+
+         System.Console.WriteLine(user.Username);
+
+
+      }
+   }
+}
---
+++ Sec02/Sec02/PointsAndLines/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PointsAndLines
+{
+   class User
+   {
+      private string username;
+      private int password;
+
+      public string Username
+      {
+         get
+         {
+            return username;
+         }
+
+      }
+
+      public int Password
+      {
+
+         set
+         {
+            if (value > 4 && value < 10)
+            {
+               password = value;
+            }
+            else
+            {
+               System.Console.WriteLine("Oops, this is not a value password!\n Please enter a password between 4 and 10 digits");
+            }
+         }
+      }
+
+      public User()
+      {
+
+      }
+
+      public User(string username)
+      {
+         this.username = username;
+
+      }
+   }
+}
